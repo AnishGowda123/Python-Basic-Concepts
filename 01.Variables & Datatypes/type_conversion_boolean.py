b89e75f280d02f5288c7137ff3658ac80fb8f9e3
Anish M S <ams631829@gmail.com>
2026-02-27 19:29:43 +0530

    Add files via upload

---
+++ 01.Variables & Datatypes/type_conversion_boolean.py
@@ -0,0 +1,17 @@
+a=10.5;
+b=True;
+c=10+2j;
+d="py";
+e=100;
+c1=bool(a);
+c2=bool(b);
+c3=bool(c);
+c4=bool(d);
+c5=bool(e);
+# anything is true except false and 0
+print("-----------------------------------------");
+print(c1);
+print(c2);
+print(c3);
+print(c4);
+print(c5);
---
+++ 01.Variables & Datatypes/type_conversion_complex.py
@@ -0,0 +1,15 @@
+a=10.5;
+b=True;
+c=10+2j;
+d="py";
+e=100;
+d1=complex(a);
+d2=complex(b);
+d3=complex(c);
+d5=complex(e);
+# all can convert except string
+print("-----------------------------------------");
+print(d1);
+print(d2);
+print(d3);
+print(d5);
