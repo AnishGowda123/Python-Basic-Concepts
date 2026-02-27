53d756895a13edb275ec93f7a56a4531fd222e3e
Anish M S <ams631829@gmail.com>
2026-02-27 19:29:19 +0530

    Add files via upload

---
+++ 01.Variables & Datatypes/type_conversion_float.py
@@ -0,0 +1,13 @@
+a=10.5;
+b=True;
+c=10+2j;
+d="py";
+e=100;
+b1=float(a);
+b2=float(b);
+b5=float(e);
+# comple cant convert into float
+print("-----------------------------------------");
+print(b1);
+print(b2);
+print(b5);
