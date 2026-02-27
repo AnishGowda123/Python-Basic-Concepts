b2c954b0ae042e3b60e6da8004a92a9d11faf5aa
Anish M S <ams631829@gmail.com>
2026-02-27 19:31:22 +0530

    Add files via upload

---
+++ 02.Operator & Expression/Membership_expression.py
@@ -0,0 +1,12 @@
+s="python";
+j="java";
+
+print(a in j);
+print(a  not in j);
+print(p in j);
+print(p not in s);
+# if the variable content certain object it show True or false
+
+
+
+
---
+++ 02.Operator & Expression/logical_expression.py
@@ -0,0 +1,7 @@
+a=20;
+b=10;
+
+print(a==b and b==a);
+#both the expression should be true
+print(a<b or a>b);
+#one of the expression should be true
