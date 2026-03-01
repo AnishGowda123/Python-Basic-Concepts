f061a96b5c8649da13db4c357a109c6476d6a256
Anish M S <ams631829@gmail.com>
2026-03-01 20:52:51 +0530

    Add files via upload

---
+++ 04.Looping Statments/for_loop_2.py
@@ -0,0 +1,13 @@
+number=int(input("Enter the number:"))
+c=int(input("Enter the count:"))
+for i in range(1,number+1):
+    if number%2==0:
+        c=c+1
+if c==2:
+    print("its a prime number")
+else:
+    print("its not a prime number")
+
+    
+    
+      
