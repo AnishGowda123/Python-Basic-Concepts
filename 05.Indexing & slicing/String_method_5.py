2cfe682df7d511e6f6d46b5b7eec0bf22633d560
Anish M S <ams631829@gmail.com>
2026-03-03 20:38:27 +0530

    Add files via upload

---
+++ 05.Indexing & slicing/String_method_5.py
@@ -0,0 +1,16 @@
+s=" python is easy language"
+m1=s.startswith("python")
+m2=s.splitlines("Python")
+print(m1)
+print(m2)
+print("--------------------------")# startswith method
+m3=s.endswith("language")
+m4=s.endswith("Language")
+print(m3)
+print(m4)
+print("--------------------------")# endswith method
+m5=s.removeprefix()
+print(m5)
+print("--------------------------")
+m6=s.removesuffix()
+print(m6)
