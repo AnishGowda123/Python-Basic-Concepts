6775ee5a575266f22829c77e6195ac21106cc217
Anish M S <ams631829@gmail.com>
2026-03-03 20:38:07 +0530

    Add files via upload

---
+++ 05.Indexing & slicing/String_method_4.py
@@ -0,0 +1,19 @@
+s="banish"
+m= s.replace("b","m")
+print(m)
+print("-------------------------")# replace method
+s1=a-b-c-d-e
+m1=s.replace("-",",",3)
+print(m1)
+print("-------------------------")# replace method
+st1="ABC"
+st2="DEF"
+m2=st1.join(st2)
+print(m2)
+print("-------------------------")# join method
+sp="python is a easy lang"
+m4=sp.split
+print(m4)
+print("-------------------------")# split method
+
+
