482466a48a568bd5b37411b7528ddb27257697ab
Anish M S <ams631829@gmail.com>
2026-03-03 20:36:56 +0530

    Add files via upload

---
+++ 05.Indexing & slicing/String_method_1.py
@@ -0,0 +1,17 @@
+s="python is easy"
+m1=s.find("s")
+print(m1)
+print("*******************")# find method
+m2=s.rfind("s")
+print(m2)
+print("*******************")# rfind method
+m3=s.index("o")
+print(m3)
+print("*******************")# index method
+m4=s.rindex("s")
+print(m4)
+print("*******************")# rindex method
+m5=s.count("s")
+print(m5)
+print("*******************")# count method
+
