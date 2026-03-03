f0dca03595fcca83700c1853a26f5df7a4bd03fc
Anish M S <ams631829@gmail.com>
2026-03-03 20:37:30 +0530

    Add files via upload

---
+++ 05.Indexing & slicing/String_method_2.py
@@ -0,0 +1,13 @@
+s="python"
+m1=s.ljust(10,"*")
+print(m1)
+print("---------------------------------------")#ljust method
+m2=s.rjust(10,"*")
+print(m2)
+print("---------------------------------------")#rjust method
+m3=s.center(10,"*")
+print(m3)
+print("---------------------------------------")# center method
+m4=s.zfill(10)
+print(m4)
+print("---------------------------------------")# zfill method
