bb30c6fcfbdb0a4b239d5250b5e933d576fcd78b
Anish M S <ams631829@gmail.com>
2026-03-03 20:37:48 +0530

    Add files via upload

---
+++ 05.Indexing & slicing/String_method_3.py
@@ -0,0 +1,10 @@
+s="python is one the lang in python java is easy"
+m1=s.lstrip("python")
+print(m1)
+print("---------------------------------------")# lstrip
+m2=s.rstrip("easy")
+print(m2)
+print("---------------------------------------")# rstrip method
+m3=s.strip("is")
+print(m3)
+print("---------------------------------------")# strip method
