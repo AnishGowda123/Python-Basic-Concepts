5d936ca06892042383dd74aa2e400a09792a6718
Anish M S <ams631829@gmail.com>
2026-03-03 20:48:39 +0530

    Add files via upload

---
+++ 05.Indexing & slicing/String_method_6.py
@@ -0,0 +1,23 @@
+s="python is a easy language"
+m1=s.partition("is")
+print(m1)
+print("-------------------------")# partition method
+m2=s.rpartition("easy")
+print(m2)
+print("-------------------------")# rpartition method
+s1="hello world"
+m3=s1.capitalize()
+print(m3)
+print("-------------------------")# capitalize method
+m4=s1.upper()
+print(m4)
+print("-------------------------")# upper method
+m5=s1.lower()
+print(m5)
+print("-------------------------")# lower method
+m6=s1.swapcase()
+print(m6)
+print("-------------------------")# swapcase method
+m7=s1.casefold()
+print(m7)
+print("-------------------------")# casefold method
